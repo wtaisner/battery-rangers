f10eb5a16a747d920817e66c059fdf9bf78a1658
wtaisner <taisner.witold@gmail.com>
2025-04-07 06:44:10 +0000

    enhance FlatnessFilter to optionally return flatness values of molecules

--- modules/core/features/filters/flatness_filter.py
+++ modules/core/features/filters/flatness_filter.py
@@ -8,18 +8,25 @@ from modules.core.features.flatness import get_flatness_mol
 class FlatnessFilter(GenericMoleculeFilter):
     """Filter that leaves molecules with flatness."""
 
-    def apply(self, molecules: list[Mol], **kwargs) -> list[Mol]:
+    # pylint: disable=arguments-differ
+    def apply(self, molecules: list[Mol], return_flatness: bool = False) -> list[Mol] | list[tuple[float, Mol]]:
         """
         Apply the filter to a list of RDKit molecules.
 
         Args:
             molecules (list[Mol]): The list of RDKit molecules to filter.
+            return_flatness (bool): If True, return the flatness values of the molecules.
         Returns:
-            list[Mol]: The list of RDKit molecules that passed the filter.
+            if return_flatness is True:
+                list[tuple[float, Mol]]: A list of tuples containing the flatness value and the corresponding RDKit molecule.
+            else:
+                list[Mol]: The list of RDKit molecules that passed the filter.
         """
         flatness = []
         for mol in molecules:
             f = get_flatness_mol(mol, plot_visualization=False, sample_size=20)
             flatness.append((f, mol))
         flatness = sorted(flatness, key=lambda x: x[0])
+        if return_flatness:
+            return flatness
         return [x[1] for x in flatness]
