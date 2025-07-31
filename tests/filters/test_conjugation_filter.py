5278c0971f1fb7ffc992e0697b5b671ba30a4878
wtaisner <taisner.witold@gmail.com>
2025-07-31 09:28:23 +0000

    refactor ConjugationFilter to enhance logic for conjugation criteria and improve documentation

--- tests/filters/test_conjugation_filter.py
+++ tests/filters/test_conjugation_filter.py
@@ -8,11 +8,11 @@ from modules.core.filters.conjugation_filter import ConjugationFilter
 @pytest.mark.parametrize(
     "smiles, expected",
     [
+        ([], []),
         (
             ["N#Cc1ccnc(C#N)n1", "N#Cc1cc(C#N)c(F)c(C#N)c1F", "N#Cc1ccc(-c2cc(=O)nc(-c3ccc(C#N)cc3)[nH]2)cc1"],
             ["N#Cc1ccnc(C#N)n1", "N#Cc1cc(C#N)c(F)c(C#N)c1F", "N#Cc1ccc(-c2cc(=O)nc(-c3ccc(C#N)cc3)[nH]2)cc1"],
         ),
-        ([], []),
         (["CCC", "CCCCC"], []),
         (
             ["Cc1ccc(C#N)c(SCC#N)n1", "N#CCC#N", "N#Cc1c2c(c(C#N)c3ccccc13)CCCC2", "N#Cc1ccc2oc(-c3cc4ccc(C#N)cc4[nH]3)cc2c1"],
