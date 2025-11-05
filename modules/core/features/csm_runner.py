5646efaf9b24871e7cafc1ae440e6a820764493c
wtaisner <taisner.witold@gmail.com>
2025-11-05 11:37:27 +0000

    reduce max attempts and number of conformers in compute_conformer function for improved performance

--- modules/core/features/csm_runner.py
+++ modules/core/features/csm_runner.py
@@ -207,7 +207,7 @@ class CSMRunner:
         host_input_path = os.path.join(self.host_data_dir, input_filename)
 
         # add increased values for CSM (thus node molecules)
-        molecule = compute_conformer(molecule=molecule, save_file=True, max_attempts=100, num_conformers=1000, filename=host_input_path)
+        molecule = compute_conformer(molecule=molecule, save_file=True, max_attempts=10, num_conformers=100, filename=host_input_path)
 
         if molecule is None:
             return None
